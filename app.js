e6fb3d5af1bfa56e3d765fbf9bed5b98d59e5897
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-09-14 23:04:17 -0500

    First commit.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(require, response, nextFn) {
+  response.send('Hey I am working!')
+  nextFn()
+})
+
+app.get('/ping', function (require, response, nextFn) {
+  response.send("PONG This should work")
+  nextFn()
+})
+
+app.listen(3000, function() {
+  console.log('This is my server!')
+})
